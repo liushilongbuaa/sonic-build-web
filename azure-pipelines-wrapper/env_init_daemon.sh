1087d3f033267a4b03a9dc1477ccfd45ba74e1c3
Liu Shilong <shilongliu@microsoft.com>
2024-04-28 21:24:28 +0800

    sleep 200 in daemon

--- azure-pipelines-wrapper/env_init_daemon.sh
+++ azure-pipelines-wrapper/env_init_daemon.sh
@@ -7,3 +7,5 @@ find . -maxdepth 2 -name "tmp.*" -type d -ctime +30 -delete
 if (( "$(df -h | grep '% /home' | awk '{print$5}' | grep -Eo [0-9]*)" > "60"));then
     find . -maxdepth 2 -name "tmp.*" -type d -ctime +20 -delete
 fi
+
+sleep 200
