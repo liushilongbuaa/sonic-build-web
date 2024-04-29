98b391f1acbaae06a1000542d16b6cba9152cf86
Liu Shilong <shilongliu@microsoft.com>
2024-04-29 19:59:56 +0800

    add daemon, fs.mkdir, ghclient

--- azure-pipelines-wrapper/env_init.js
+++ azure-pipelines-wrapper/env_init.js
@@ -1,6 +1,7 @@
-const spawnSync = require('child_process').spawnSync;
 const execFile = require('child_process').execFile;
-const lockfile = require('proper-lockfile');
+const akv = require('./keyvault.js');
+const { App, createNodeMiddleware } = require("@octokit/app");
+const fs = require('fs');
 
 async function init(app){
     while ( true ){
@@ -17,19 +18,39 @@ async function init(app){
 
 async function daemon_run(app){
     setInterval(async function() {
-        lockfile.lock('env_init_daemon.log')
-        .then((release) => {
-            app.log.info("[ DAEMON ] Start to run daemon process")
-            execFile('./env_init_daemon.sh', { encoding: 'utf-8' }, (error)=>{
-                app.log.info(error)
-                release()
-            })
+        try {
+            fs.mkdirSync('daemon_lock')
+        } catch(e) {
+            if(e.code == 'EEXIST') {
+                return
+            }
+            throw(e)
+        }
+        const privateKey = await akv.getAppPrivateKey();
+        const secret = await akv.getAppWebhookSecret();
+        let appclinet = new App({
+            appId: process.env.APP_ID,
+            privateKey: privateKey,
+            webhooks: {
+                secret: secret,
+            },
         })
-        .catch((e) => {
-            // either lock could not be acquired
-            console.log(e)
-        });
-    }, 300000);
+        let data = await appclinet.octokit.request("/app");
+        app.log.info(["[ DAEMON ] START!", data.data.name].join(" "));
+        execFile('./env_init_daemon.sh', { encoding: 'utf-8' }, (error, stdout)=>{
+            for (const line of stdout.split(/\r?\n/)){
+                if (line.includes("ms_conflict.result: ")){
+                    let pr_result = line.split(' ').pop()
+                    if (pr_result.split('=').length == 2){
+                        let pr = pr_result.split('=')[0]
+                        let result = pr_result.split('=')[1]
+                        app.log.info(["[ DAEMON ]", pr, result].join(" "));
+                    }
+                }
+            }
+        })
+        fs.rmdirSync("daemon_lock");
+    }, 30000);
 };
 
 module.exports = {
--- azure-pipelines-wrapper/env_init_daemon.sh
+++ azure-pipelines-wrapper/env_init_daemon.sh
@@ -1,11 +1,27 @@
-#!/bin/bash -ex
+#!/bin/bash -e
 
-echo "$(date '+%FT%TZ'): daemon script start!" >> env_init_daemon.log
+echo "$(date '+%FT%TZ'): daemon script start!" &>> env_init_daemon.log
 cd workspace
-find . -maxdepth 2 -name "tmp.*" -type d -ctime +30 -delete
+find . -maxdepth 2 -name "tmp.*" -type d -ctime +30 -delete &>> env_init_daemon.log
 
 if (( "$(df -h | grep '% /home' | awk '{print$5}' | grep -Eo [0-9]*)" > "60"));then
-    find . -maxdepth 2 -name "tmp.*" -type d -ctime +20 -delete
+    find . -maxdepth 2 -name "tmp.*" -type d -ctime +20 -delete &>> env_init_daemon.log
 fi
 
-sleep 200
+cd conflict-sonic-buildimage
+mkdir -p daemon
+touch daemon/done.list
+
+grep PR_NUMBER $(find . -maxdepth 2 -name .bashenv -mtime -1 -mmin +120) | awk -F/.bashenv:PR_NUMBER= '{print$1,$2}' | sort -k 2 -u | awk '{print$1}' | grep -v -f daemon/done.list > daemon/todo.list
+cd daemon
+for i in $(cat todo.list); do
+    grep FORCE_PUSH=true ../$i/.bashenv || continue
+    mkdir $i || continue
+    cp ../$i/.bashenv ../$i/script.sh $i/
+    cd $i
+    echo ACTION=ms_checker >> .bashenv
+    ./script.sh | sed "s/ms_checker.result: /ms_checker.result: $PR_NUMBER=/" | while IFS= read -r line; do echo "[$(date '+%FT%TZ')] $line" > tmp
+    cat tmp
+    echo $i >> done.list
+    sleep 2
+done
