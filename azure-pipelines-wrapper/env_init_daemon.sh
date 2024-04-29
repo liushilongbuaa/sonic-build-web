d61475e4f5e24295fe5f9c4fe059c3a3f1fe5904
Liu Shilong <shilongliu@microsoft.com>
2024-04-29 21:32:25 +0800

    find || true

--- azure-pipelines-wrapper/env_init_daemon.sh
+++ azure-pipelines-wrapper/env_init_daemon.sh
@@ -1,11 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 echo "$(date '+%FT%TZ'): daemon script start!" &>> env_init_daemon.log
 cd workspace
-find . -maxdepth 2 -name "tmp.*" -type d -ctime +30 -delete &>> env_init_daemon.log
+find . -maxdepth 2 -name "tmp.*" -type d -ctime +30 -delete &>> env_init_daemon.log || true
 
 if (( "$(df -h | grep '% /home' | awk '{print$5}' | grep -Eo [0-9]*)" > "60"));then
-    find . -maxdepth 2 -name "tmp.*" -type d -ctime +20 -delete &>> env_init_daemon.log
+    find . -maxdepth 2 -name "tmp.*" -type d -ctime +20 -delete &>> env_init_daemon.log || true
 fi
 
 cd conflict-sonic-buildimage
